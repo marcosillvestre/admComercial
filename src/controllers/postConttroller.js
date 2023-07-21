512c2921005436b58d80f97525f651e4ea19be89
marcos vinicius <marcos.vinicius7170@gmail.com>
2023-07-21 17:26:19 -0300

    acdata fixes

--- src/controllers/postConttroller.js
+++ src/controllers/postConttroller.js
@@ -55,7 +55,7 @@ class PostController {
                         "ppStatus": "Pendente",
 
 
-                        "dataAC": [{}],
+                        "dataAC": ["Pendente"],
                         "formatoAula": res.data.data.customFields.formato_de_aula.value,
                         "tipoModalidade": res.data.data.customFields.tipo_modalidade.value,
                         "professor": res.data.data.customFields.professor.map(res => res.value),
