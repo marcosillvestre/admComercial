e64ab4aa2ba8c9c72bad929886d18ea3c7a5fcd5
marcos vinicius <marcos.vinicius7170@gmail.com>
2023-07-24 11:12:45 -0300

    fix-db03

--- src/controllers/postConttroller.js
+++ src/controllers/postConttroller.js
@@ -55,7 +55,7 @@ class PostController {
                         "ppStatus": "Pendente",
 
 
-                        "dataAC": { set: obj },
+                        "dataAC": { obj },
                         "formatoAula": res.data.data.customFields.formato_de_aula.value,
                         "tipoModalidade": res.data.data.customFields.tipo_modalidade.value,
                         "professor": res.data.data.customFields.professor.map(res => res.value),
