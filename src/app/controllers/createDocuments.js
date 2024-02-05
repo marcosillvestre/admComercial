9a067bef41a0dfa94fb778bb25391eab55aefaa1
marcos vinicius <marcos.vinicius7170@gmail.com>
2024-02-05 16:26:31 -0300

    puppeteer configs args

--- src/app/controllers/createDocuments.js
+++ src/app/controllers/createDocuments.js
@@ -80,7 +80,7 @@ class CreateDocuments {
             }
 
 
-            const browser = await puppeteer.launch({ headless: 'new' })
+            const browser = await puppeteer.launch({ headless: 'new', args: ["--no-sandbox", "--disable-setuid-sandbox"], })
             const page = await browser.newPage()
 
             const content = await compiler(info.subclasse, info)
